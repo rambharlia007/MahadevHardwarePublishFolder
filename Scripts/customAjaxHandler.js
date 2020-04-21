9d4bb4f7a333cf4ca13efe8f2afb47a0acaf382f
rambharlia007 <rambharlia007@gmail.com>
2020-04-21 11:42:00 +0530

    v234

---
+++ Scripts/customAjaxHandler.js
@@ -0,0 +1,37 @@
+﻿function showLoader() {
+    $(".global-spinner").removeClass("hide");
+    $(".body-content").removeClass("blur-bg");
+    $(".body-content").addClass("blur-bg");
+}
+function hideLoader() {
+    $(".global-spinner").removeClass("hide");
+    $(".body-content").removeClass("blur-bg");
+    $(".global-spinner").addClass("hide");
+}
+(function initGameIIFE() {
+    //$(document).ajaxComplete(function () {
+    //    hideLoader();
+    //});
+
+    //$(document).ajaxStart(function () {
+    //    showLoader();
+    //});
+    $(document).ajaxError(function () {
+        hideLoader();
+    });
+    $(document).ajaxStop(function () {
+        hideLoader();
+    });
+    //$(document).ajaxSuccess(function () {
+    //    hideLoader();
+    //});
+
+    $(window).on("load", function () {
+        hideLoader();
+    });
+
+    $("#navbarColor01 .nav-link").on("click", function (e) {
+        showLoader();
+    });
+
+}());
